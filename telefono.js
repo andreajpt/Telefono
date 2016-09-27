638bf723ef65ea85c3c2fb962d0ce08bdcc73392
andreajpt <andreajtp85@gmail.com>
2016-09-27 19:59:54 -0400

    actualizando actividad

--- telefono.js
+++ telefono.js
@@ -1,17 +1,17 @@
 function llamada(){
-	var llamar=prompt("Introduzca numero o nombre de contacto");
-	alert ("llamar a: " + llamar);
+	var llamar=prompt("Introduzca número o nombre de contacto: ", "");
+	alert ("llamar a: " +llamar);
 }
 
-function mensaje(){
-	var destinatario=prompt("Introduzca destinatario");
-	var mensaje=prompt("Escribir texto");
-	alert("está a punto de enviar un mensaje a: " destinatario +" con el texto:" +mensaje);
+function mensaje() {
+	var destinatario=prompt("Introduzca destinatario: ", "");
+	var mensaje=prompt("Escribir texto: ", "");
+	alert("Está a punto de enviar un mensaje a: "+destinatario+" con el texto:" +mensaje);
 } 
 
 function mail() {
-	var destinatario=prompt("Introducir correo electronico de destino");
-	var asunto=prompt("Ingrese Asunto de mensaje");
-	var mensaje=prompt("Escriba aqui su mensaje");
-	alert ("va a enviar un correo electronico a: " +destinatario +" Ingrese asunto: "+ asunto + " mensaje: " + mensaje);
+	var destinatario=prompt("Introducir correo electrónico de destino: ", "");
+	var asunto=prompt("Ingrese Asunto de mensaje: ", "");
+	var mensaje=prompt("Escriba aquí su mensaje. ", "");
+	alert ("va a enviar un correo electronico a: "+destinatario+" Ingrese asunto: "+asunto+ " mensaje: "+mensaje);
 }
